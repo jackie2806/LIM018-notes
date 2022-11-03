133bf90246994c06078b3601d7e361a1d5066c21
jackie2806 <jaishackeline@gmail.com>
2022-11-02 19:39:02 -0500

    limpiando la plantilla de React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img  className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>Hola Jackie!!! </code>
         </p>
         <a
           className="App-link"
